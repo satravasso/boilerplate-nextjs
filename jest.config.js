87b8804219f383890051e2b82cba94d11075ce50
SabrinaTravasso <53414857+SabrinaTravasso@users.noreply.github.com>
2021-02-02 18:54:40 -0300

    config jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)']
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx']
 }
